ccf5b9daf960780f84ca7a6c130648e6a3a95b5b
monirulislam14 <damfast14@gmail.com>
2019-12-09 03:05:49 +0600

    Second Commit

--- UniversityManagementWithEntityFramework/Controllers/CourseController.cs
+++ UniversityManagementWithEntityFramework/Controllers/CourseController.cs
@@ -29,5 +29,36 @@ namespace UniversityManagementWithEntityFramework.Controllers
             db.SaveChanges();
             return RedirectToAction("Index", "Course");
         }
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Departments = new SelectList(db.Departments.ToList(), "DepartmentId", "DepartmentName");
+            ViewBag.Semesters = new SelectList(db.Semesters.ToList(), "SemesterId", "SemesterName");
+            return View(db.Courses.Where(x => x.CourseId == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(Course course)
+        {
+            db.Entry(course).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index","Course");
+        }
+        public ActionResult Details(int id)
+        {
+            return View(db.Courses.Where(x => x.CourseId == id).FirstOrDefault());
+        }
+        public ActionResult Delete(int id)
+        {
+            return View(db.Courses.Where(x => x.CourseId == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Delete(int id,FormCollection collection)
+        {
+            Course course = db.Courses.Where(x => x.CourseId == id).FirstOrDefault();
+            db.Entry(course).State = EntityState.Deleted;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Course");
+        }
     }
 }
